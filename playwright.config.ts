7228ae10182e5149ab8c84c7d68804e2ad3f89e6
SKRTEEEEEE <adanreh.m@gmail.com>
2025-10-25 22:18:58 +0200

    build(migration): base from profile-next

---
+++ playwright.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',          // Carpeta donde están tus tests
+  testMatch: '*.spec.ts',      // Patrón de archivos
+  timeout: 30000,              // Timeout por test
+  retries: 2,                  // Reintentos si falla
+  outputDir: "docs/test-results",
+  use: {
+    headless: true,            // Ejecutar en headless
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+  },
+});
---
+++ src/libs/i18n/routing.ts
@@ -0,0 +1,40 @@
+import {defineRouting} from 'next-intl/routing';
+import {createNavigation} from 'next-intl/navigation';
+
+export type AllPathnamesType = keyof typeof routing.pathnames;
+
+
+export const routing = defineRouting({
+  // A list of all locales that are supported
+  locales: ['en', 'de', "es", "ca"],
+ 
+  // Used when no locale matches
+  defaultLocale: 'en',
+
+  // List of rutes translation
+  pathnames: {
+    "/":"/",
+    // "/academia":{
+    //   en: "/academy",
+    //   de: "/akademie",
+    //   es: "/academia",
+    //   ca: "/academia"
+    // },
+    // "/academia/ejercicios":"/academia/ejercicios",
+    // "/academia/ejercicios/[...slug]":"/academia/ejercicios/[...slug]",
+    // "/academia/tarifas":"/academia/tarifas",
+    // "/academia/temas-ejercicios": "/academia/temas-ejercicios",
+    // "/academia/temas-ejercicios/[tema]": "/academia/temas-ejercicios/[tema]",
+    // "/academia/verify-email":"/academia/verify-email",
+    // "/admin":"/admin",
+    // "/admin/techs":"/admin/techs",
+    // "/admin/users":"/admin/users",
+
+
+  }
+});
+ 
+// Lightweight wrappers around Next.js' navigation APIs
+// that will consider the routing configuration
+export const {Link, redirect, usePathname, useRouter, getPathname} =
+  createNavigation(routing);
